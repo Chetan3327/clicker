b9317e2266f239c250eb9c205299be39b6fca85f
Chetan3327 <chauhanchetan12789@gmail.com>
2023-05-21 11:34:34 +0530

    added reset button

---
+++ newscript.js
@@ -0,0 +1,36 @@
+if(localStorage.clickcount === undefined && localStorage.autoInc === undefined){
+    localStorage.clickcount = 1;
+    localStorage.autoInc = 0;   
+}
+document.getElementById("score").innerText = localStorage.clickcount;
+
+
+function update(){
+    document.getElementById("score").innerText = localStorage.clickcount;
+    document.getElementById("inc").innerText = "Auto Inc: " + localStorage.autoInc;
+
+    localStorage.clickcount = Number(localStorage.clickcount) + Number(localStorage.autoInc)
+}
+function c(){
+    localStorage.clickcount = Number(localStorage.clickcount) + 1
+    console.log("object");
+}
+
+
+
+
+function Inc(cost, inc){
+    if(Number(localStorage.clickcount) > Number(cost)){
+        localStorage.clickcount = Number(localStorage.clickcount) - Number(cost)
+        localStorage.autoInc = Number(localStorage.autoInc) + Number(inc)
+    }
+}
+
+setInterval(update, 1000)
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const button = document.getElementById("click")
 const score = document.getElementById("score")
+
+// let clickcount = localStorage.clickcount
+// let autoInc = localStorage.autoInc
+
+
+
+
 window.onload = function(){
     update()
 }
@@ -9,8 +16,30 @@ function update(){
         localStorage.clickcount = Number(localStorage.clickcount) + 1;
     } else {
         localStorage.clickcount = 1;
+        localStorage.autoInc = 0;
         document.getElementById("score").innerText = localStorage.clickcount;
     }
     document.getElementById("score").innerText = localStorage.clickcount;
 }
+
+
+function mainLoop(){
+    document.getElementById("score").innerText = localStorage.clickcount;
+    localStorage.clickcount = Number(localStorage.clickcount) + Number(localStorage.autoInc)
+    // localStorage.clickcount = Number(localStorage.clickcount) + 1;
+}
+
+setInterval(mainLoop, 1000);
+
+function autoInc(cost, inc){
+    if(Number(localStorage.clickcount) >= cost){
+        localStorage.autoInc = Number(localStorage.autoInc) + inc;
+        localStorage.clickcount = Number(localStorage.clickcount) - cost;
+    }
+}
+
+
+
+
+
 
